664257296c9bfafaf9aea34f8155e9861034489d
HALLODEERE <twaspace@hotmail.com>
2019-02-06 15:10:13 -0500

    Added the Unity files in the folder yikes_i_fell_unity

---
+++ yikes_i_fell_unity/Assets/MovementScript.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MovementScript : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) //this code is legacy from testing with a cylinder, I couldn't get collisions to work so retrying with a Rigidbody thingy
+        {
+            Vector3 position = this.transform.position;
+            position.z--;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.z++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x--;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+    }*/
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 100;
+        }
+        else
+        {
+            speed = 7;
+        }
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        Debug.Log(speed);
+        Debug.Log(Input.GetKeyDown(KeyCode.LeftShift));
+    }
+
+    public void Jump()
+    {
+        rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+    }
+}
