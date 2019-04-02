3e2b71d87ad2c7e3bd2727593502f850b8522683
justness <vc00@ymail.com>
2019-04-02 03:22:28 -0400

    interactable key, bed, locked door

---
+++ yikes_i_fell_unity/Assets/InteractDoor.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractDoor : MonoBehaviour
+{
+    public bool enter = true;
+    public bool stay = true;
+    public bool exit = true;
+    public string message;
+    public bool toggle = false;
+    public GameObject text;
+    public Toggle door;
+
+    void OnGUI()
+    {
+        GUI.enabled = toggle;
+        if (toggle == false)
+        {
+            GUI.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        }
+        if (toggle == true)
+        {
+            GUI.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        }
+        GUI.Box(new Rect(0, Screen.height - 100, Screen.width, 100), message);
+        GUI.contentColor = Color.white;
+        GUI.skin.label.alignment = TextAnchor.UpperLeft;
+    }
+
+    private void Start()
+    {
+        door = GetComponent<Toggle>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("entered");
+    }
+
+    private float stayCount = 0.0f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (stay)
+        {
+            stayCount = stayCount + Time.deltaTime;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                toggle = true;
+                Debug.Log("Space pressed.");
+            }
+     
+            else { }
+
+            if (stayCount > 0.25f)
+            {
+                Debug.Log("staying");
+                stayCount = stayCount - 0.25f;
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        toggle = false;
+
+        if (exit)
+        {
+            Debug.Log("exited");
+        }
+    }
+
+}
---
+++ yikes_i_fell_unity/Assets/InteractKey.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractKey : MonoBehaviour
+{
+    public bool enter = true;
+    public bool stay = true;
+    public bool exit = true;
+    public string message;
+    public bool toggle = false;
+    //public GameObject keycoll;
+    public GameObject text;
+
+    void OnGUI()
+    {
+        GUI.enabled = toggle;
+        if (toggle == false)
+        {
+            GUI.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        }
+        if (toggle == true)
+        {
+            GUI.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        }
+        GUI.Box(new Rect(0, Screen.height - 100, Screen.width, 100), message);
+        GUI.contentColor = Color.white;
+        GUI.skin.label.alignment = TextAnchor.UpperLeft;
+    }
+
+    private void Start()
+    {
+        /*keycoll = GameObject.Find("Key").GetComponent<GameObject>();
+        keycoll.GetComponent<Text>().enabled = false;*/
+        text = GameObject.Find("pickupText");
+        text.GetComponent<Text>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("entered");
+        text.GetComponent<Text>().enabled = true;
+        //keycoll.GetComponent<Text>().enabled = true;
+    }
+
+    private float stayCount = 0.0f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (stay)
+        {
+            stayCount = stayCount + Time.deltaTime;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                /*Text flav = transform.gameObject.AddComponent<Text>();
+                flav.color = Color.white;
+                flav.alignment = TextAnchor.UpperLeft;
+                flav.fontSize = 10;
+                //flav.transform.SetParent(transform);
+                flav.text = message;*/
+                toggle = true;
+                Debug.Log("Space pressed.");
+            }
+
+            if (stayCount > 0.25f)
+            {
+                Debug.Log("staying");
+                stayCount = stayCount - 0.25f;
+            }
+            else
+            {
+                
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        toggle = false;
+
+        if (exit)
+        {
+            //keycoll.GetComponent<Text>().enabled = false;
+            text.GetComponent<Text>().enabled = false;
+            Debug.Log("exited");
+        }
+    }
+
+}
