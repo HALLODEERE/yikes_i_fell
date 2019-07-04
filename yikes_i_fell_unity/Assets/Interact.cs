8ee20b4375de3806a27ace0c6f7013efede155a1
Ness <vanessa.chu@mail.mcgill.ca>
2019-07-04 19:31:24 -0400

    Setup blank general Interact script
    I think our gitignore might not work, since our actual project is a second folder within the overall yikes_i_fell folder...

---
+++ yikes_i_fell_unity/Assets/Interact.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//GENERIC INTERACT SCRIPT
+//Takes OBJECT. (GameObject.Find(""))
+//Checks player and object COLLIDERS. (Object.GetComponent?)
+//Outputs UI TEXT. (conditional) --> public String message (no need for GameObject text)
+//Calls next method if key object. (conditional)
+
+//i.e. open interactive UI (no flavor text) if console
+//i.e. prints observe flavour text with no next method if locked door
+//i.e. prints observe flavour text and begins dialogue if NPC
+
+public class Interact : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
