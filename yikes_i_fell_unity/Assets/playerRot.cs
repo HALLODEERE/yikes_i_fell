fe5bbe0a1e564da3125d0641be15a998c1f5be5b
filrod <filipe.rodrigues@mail.mcgill.ca>
2019-11-26 18:42:51 -0500

    temp fix for broken rotation. movement broken on main branch

---
+++ yikes_i_fell_unity/Assets/playerRot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerRot : MonoBehaviour
+{
+
+    //public Rigidbody rb;
+    public float rotationSpeed = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 dir = rb.velocity;
+        //transform.rotation = Quaternion.LookRotation(dir);
+
+            float rotation = (Input.GetAxis("Horizontal")+Input.GetAxis("Vertical")/2) * rotationSpeed - 0.2f;
+            rotation *= Time.deltaTime;
+            transform.Rotate(0, rotation, 0);
+    }
+}
